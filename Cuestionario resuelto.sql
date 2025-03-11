d38892c89427f940be7bc42512d8d36d1f7ebde9
Giovanni Fermin <giovannifermin8056@gmail.com>
2025-03-11 15:54:07 -0400

    Tres paises con mas radioayudas registradas
    Se realiza la pregunta numero 5 del cuestionario

--- Cuestionario resuelto.sql
+++ Cuestionario resuelto.sql
@@ -7,4 +7,6 @@
 --4.	¿Cuántos aeropuertos tienen servicio programado en América del Norte?
 
 --5.	Encuentra los 3 países con más radioayudas registradas.
-
+SELECT navaids.iso_country AS 'codigo_del_pais', countries.name AS 'Pais', count(navaids.type) AS
+'Radioayudas_registradas' FROM navaids INNER JOIN countries ON navaids.iso_country=countries.code 
+GROUP BY iso_country ORDER BY count(navaids.type) DESC LIMIT 3; 
