ef6082aca2c08b69a18b1ae4c799b79be0d78a9b
Giovanni Fermin <giovannifermin8056@gmail.com>
2025-03-11 15:49:36 -0400

    Query del cuestionario
    Se procede a subir el query de SQLite para poder realizar las consultas pertinentes

---
+++ Cuestionario resuelto.sql
@@ -0,0 +1,10 @@
+--1.	¿Cuántos aeropuertos hay en cada país y cuál es el país con más aeropuertos registrados?
+
+--2.	¿Cuál es el aeropuerto con la mayor elevación en la base de datos?
+
+--3.	Lista los 5 aeropuertos con las pistas más largas.
+
+--4.	¿Cuántos aeropuertos tienen servicio programado en América del Norte?
+
+--5.	Encuentra los 3 países con más radioayudas registradas.
+
