5801094fecb2dcbed141f8fa4f5f7e9324dfda22
Geremi Asael Cardenas <geremicardenas72@gmail.com>
2025-03-11 17:59:32 -0400

    Se realiza la pregunta 1 del cuestionario.
    La respuesta de cuántos aeropuertos hay por país se puede ver al ejecutar la vista creada. El país con más aeropuertos registrados es Estados Unidos, con 31.690 aeropuertos a nivel nacional.

--- Cuestionario resuelto.sql
+++ Cuestionario resuelto.sql
@@ -1,5 +1,8 @@
 --1.	¿Cuántos aeropuertos hay en cada país y cuál es el país con más aeropuertos registrados?
-
+CREATE VIEW Aereopuertos_por_pais AS
+SELECT c.name AS Nombre_pais, COUNT(a.id) AS Cant_aereopuertos FROM airports a
+JOIN countries c ON a.iso_country = c.code GROUP BY c.name
+ORDER BY  Cant_aereopuertos DESC;
 --2.	¿Cuál es el aeropuerto con la mayor elevación en la base de datos?
 
 --3.	Lista los 5 aeropuertos con las pistas más largas.
